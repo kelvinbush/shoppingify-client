617dfd3a9def12c315f73ef4739a77f5c7d231a5
Kelvin Bush <kelybush@gmail.com>
2022-02-17 13:15:58 +0300

    setting up the layout complete

--- src/layouts/home/home.tsx
+++ src/layouts/home/home.tsx
@@ -1,12 +1,14 @@
 import styles from "./home.module.scss";
 import Sidebar from "../sidebar/sidebar";
 import MainContent from "../main-content/main_content";
+import ItemSection from "../item-section/item_section";
 
 export default function HomePage() {
   return (
     <div className={styles.container}>
       <Sidebar />
       <MainContent />
+      <ItemSection />
     </div>
   );
 }
--- src/layouts/item-section/item_section.tsx
+++ src/layouts/item-section/item_section.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
-import styles from "./item_section.module.scss"
+import styles from "./item_section.module.scss";
+import AddListItem from "../../components/list-item";
 
 export default function ItemSection() {
   return (
     <section className={styles.details}>
       <div>
         <div className={styles.add}>
-          <Image src="img/source.svg" alt="Source image" />
+          <img
+            className={styles.add__source}
+            src={"/img/source.svg"}
+            alt="Source image"
+          />
           <div className={styles.add__container}>
             <p>Didn't find what you need?</p>
             <button>Add item</button>
@@ -18,9 +23,9 @@ export default function ItemSection() {
             <use xlinkHref={"/img/sprite.svg#icon-edit-pencil"} />
           </svg>
         </div>
-        <div className={styles.my__items}></div>
+        <AddListItem />
       </div>
-      <div className={styles.my__items__actions}>
+      <div className={styles.details__actions}>
         <input placeholder="Enter a name" type="text" />
         <button>Save</button>
       </div>
